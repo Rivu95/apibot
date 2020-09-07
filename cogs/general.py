33e3b591b0e345399ea24f3b7f81b44a074c176c
Niladri Ghosh <43736801+Rivu95@users.noreply.github.com>
2020-09-07 10:57:57 +0530

    Added js and py command to get links of guide and wrapper

--- cogs/general.py
+++ cogs/general.py
@@ -26,6 +26,16 @@ class General(commands.Cog):
         """Responds with a link to a Discord message on the Discord Link API (by ReverendMike)"""
         await ctx.send("https://discordapp.com/channels/566451504332931073/681617252814159904/685533375280578610")
 
+    @commands.command(name="js")
+    async def js(self, ctx):
+        """Responds with links of discord.js guide and coc api wrapper made by Suvajit"""
+        await ctx.send("Discord js Guide: https://discordjs.guide/\nJS CoC Api Wrapper: https://www.npmjs.com/package/clashofclans.js")
+
+    @commands.command(name="py")
+    async def js(self, ctx):
+        """Responds with links of discord.py guide and coc api wrapper made by Mathsman"""
+        await ctx.send("Discord py Guide: https://pythondiscord.com/pages/resources/guides/\nPythion CoC Api Wrapper: https://cocpy.readthedocs.io/en/latest/")
+
 
 def setup(bot):
     bot.add_cog(General(bot))
